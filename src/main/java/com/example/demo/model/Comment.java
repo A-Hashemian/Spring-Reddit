6d64af090e629a4c4a7ffcc3b7b6a5bc0921d450
A-Hashemian <amin@hashemian.tech>
2022-09-08 11:18:59 +0300

    upx_04

--- src/main/java/com/example/demo/model/Comment.java
+++ src/main/java/com/example/demo/model/Comment.java
@@ -11,5 +11,21 @@ import java.time.Instant;
 import static javax.persistence.FetchType.LAZY;
 import static javax.persistence.GenerationType.IDENTITY;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String text;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
 }
