852349b10dd372a996d41be3f5e66f0e5270793e
A-Hashemian <amin@hashemian.tech>
2022-09-10 22:29:16 +0300

    upx_32

---
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.PostRequest;
+import com.example.demo.dto.PostResponse;
+import com.example.demo.service.PostService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
