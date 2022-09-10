4aec640d519b14041dee2745375dd729aabdb051
A-Hashemian <amin@hashemian.tech>
2022-09-10 22:29:55 +0300

    upx_33

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -10,4 +10,5 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
