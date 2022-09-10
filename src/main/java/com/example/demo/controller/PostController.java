c44bdcb8e56c9968735ab87b183667668d8db26a
A-Hashemian <amin@hashemian.tech>
2022-09-10 22:30:41 +0300

    upx_34

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -12,3 +12,16 @@ import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
+
+
+@RestController
+@RequestMapping("/api/comments/")
+@AllArgsConstructor
+public class CommentsController {
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<Void> createComment(@RequestBody CommentsDto commentsDto) {
+        commentService.save(commentsDto);
+        return new ResponseEntity<>(CREATED);
+    }
